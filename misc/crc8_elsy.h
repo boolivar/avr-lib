84285998179e62c8adcfaf9470e9d87aaaaefc68
bool <boolivar@gmail.com>
2013-02-04 18:33:49 +0300

    Обновлены функции модуля crc8 - добавлена функция для подсчета на лету

--- misc/crc8_elsy.h
+++ misc/crc8_elsy.h
@@ -1,6 +1,9 @@
 #ifndef CRC8_ELSY_INCLUDED
 #define CRC8_ELSY_INCLUDED
 
+extern uint8_t calc_crc(const void *data, uint8_t len);
+extern uint8_t update_crc(uint8_t data, uint8_t crc);
+
 extern unsigned char elsy_make_crc(const void *data, unsigned char num);
 
 #endif
