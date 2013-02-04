33aa8b0bae0c5d0d611bfe3f2a32c1db26cc961a
bool <boolivar@gmail.com>
2013-02-04 18:34:18 +0300

    Внесен код с индексами для очереди, выключен директивами

--- misc/queue.c
+++ misc/queue.c
@@ -1,44 +1,85 @@
 #include "queue.h"
 
-void q_create(queue_t *q, unsigned char *pool, unsigned char size);
+#include <util/atomic.h>
+
+void q_create(queue_t *q, void *pool, uint8_t size);
 void q_put(queue_t *q, q_element_t elem);
 q_element_t q_get(queue_t *q);
+void q_flush(queue_t *q);
 unsigned char q_rest(const queue_t *q);
+uint8_t q_space(const queue_t *q);
 
-void q_create(queue_t *q, unsigned char *pool, unsigned char size)
+void q_create(queue_t *q, void *pool, uint8_t size)
 {
+#if 1
     q->in = pool;
     q->out = pool;
     q->data_start = pool;
-    q->data_end = (pool + size);
+    q->size = size/sizeof(q_element_t);
+    q->data_end = (q->data_start + q->size);
     q->count = 0;
+#else
+    q->in = 0;
+    q->out = 0;
+    q->data = pool;
+    q->size = size/sizeof(q_element_t);
+    q->count = 0;
+#endif
 }
 
 void q_put(queue_t *q, q_element_t elem)
 {
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		++q->count;
+	}
+#if 1
     *q->in++ = elem;
 
     if (q->in == q->data_end) {
         q->in = q->data_start;
     }
-
-    ++q->count;
+#else
+    q->data[q->in] = elem;
+    if (++q->in == q->size) {
+    	q->in = 0;
+    }
+#endif
 }
 
 q_element_t q_get(queue_t *q)
 {
     q_element_t elem;
-
-    --q->count;
+#if 1
     elem = *q->out++;
     if (q->out == q->data_end) {
         q->out = q->data_start;
     }
+#else
+
+    elem = q->data[q->out];
+    if (++q->out == q->size) {
+    	q->out = 0;
+    }
+#endif
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		--q->count;
+	}
 
     return elem;
 }
 
-unsigned char q_rest(const queue_t *q)
-{
-    return q->count;
+void q_flush(queue_t *q) {
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		q->count = 0;
+		q->out = q->in;
+	}
+}
+
+uint8_t q_rest(const queue_t *q) {
+	return q->count;
+}
+
+uint8_t q_space(const queue_t *q) {
+	return (q->size - q->count);
 }
--- misc/queue.h
+++ misc/queue.h
@@ -1,19 +1,40 @@
 #ifndef QUEUE_H_
 #define QUEUE_H_
 
-typedef unsigned char q_element_t;
+#include <stdint.h>
+
+typedef uint8_t q_element_t;
+
+#if 1
 
 typedef struct queue_struct {
     q_element_t *in;
     q_element_t *out;
-    unsigned char count;
-    unsigned char *data_start;
-    unsigned char *data_end;
+    q_element_t *data_start;
+    q_element_t *data_end;
+    uint8_t size;
+    uint8_t count;
 } queue_t;
 
-extern void q_create(queue_t *q, unsigned char *pool, unsigned char size);
+#else
+
+typedef struct queue_struct {
+    q_element_t *data;
+
+    uint8_t in;
+    uint8_t out;
+
+    uint8_t size;
+    uint8_t count;
+} queue_t;
+
+#endif
+
+extern void q_create(queue_t *q, void *pool, unsigned char size);
 extern void q_put(queue_t *q, q_element_t elem);
 extern q_element_t q_get(queue_t *q);
+extern void q_flush(queue_t *q);
 extern unsigned char q_rest(const queue_t *q);
+extern uint8_t q_space(const queue_t *q);
 
 #endif
