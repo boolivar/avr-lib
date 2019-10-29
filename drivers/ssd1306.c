58e916e7b1d5d8750a14a935603b80ea37b010cc
Aleksey Krichevskiy <boolivar@gmail.com>
2019-10-29 23:22:54 +0300

    ssd1306 ssd1306_configure, api functions

--- drivers/ssd1306.c
+++ drivers/ssd1306.c
@@ -1,2 +1,142 @@
-#include "i2c.h"
+
 #include "ssd1306.h"
+
+i2c_state_t ssd1306_configure(ssd1306_addr_t addr, const ssd1306_config_t* cfg);
+i2c_state_t ssd1306_start(ssd1306_addr_t addr);
+i2c_state_t ssd1306_send_command2(ssd1306_command_t cmd, uint8_t data);
+i2c_state_t ssd1306_send_command(ssd1306_command_t cmd);
+i2c_state_t ssd1306_send(uint8_t value);
+void ssd1306_stop();
+
+i2c_state_t ssd1306_configure(ssd1306_addr_t addr, const ssd1306_config_t* cfg) {
+	i2c_state_t state;
+
+	do {
+		state = ssd1306_start(addr);
+		if (state != I2C_MASTER_SLAW_ACK) {
+			break;
+		}
+
+		if (cfg->display_clock != 0) {
+			state = ssd1306_send_command2(SSD1306_CLOCK_RATIO, *cfg->display_clock);
+			if (state != I2C_MASTER_TXDATA_ACK) {
+				break;
+			}
+		}
+
+		if (cfg->multiplex_ratio != 0) {
+			state = ssd1306_send_command2(SSD1306_MULTIPLEX_RATIO, *cfg->multiplex_ratio);
+			if (state != I2C_MASTER_TXDATA_ACK) {
+				break;
+			}
+		}
+
+		if (cfg->display_offset != 0) {
+			state = ssd1306_send_command2(SSD1306_DISPLAY_OFFSET, *cfg->display_offset);
+			if (state != I2C_MASTER_TXDATA_ACK) {
+				break;
+			}
+		}
+
+		if (cfg->contrast != 0) {
+			state = ssd1306_send_command2(SSD1306_DISPLAY_CONTRAST, *cfg->contrast);
+			if (state != I2C_MASTER_TXDATA_ACK) {
+				break;
+			}
+		}
+
+		if (cfg->vcomh_deselect_level != 0) {
+			state = ssd1306_send_command2(SSD1306_VCOMH_DESELECT_LEVEL, (*cfg->vcomh_deselect_level & 0x07) << 4);
+			if (state != I2C_MASTER_TXDATA_ACK) {
+				break;
+			}
+		}
+
+		if (cfg->display_start_line != 0) {
+			state = ssd1306_send_command(SSD1306_DISPLAY_START_LINE | (*cfg->display_start_line & 0x3f));
+			if (state != I2C_MASTER_TXDATA_ACK) {
+				break;
+			}
+		}
+
+		if (cfg->charge_pump != 0) {
+			state = ssd1306_send_command2(SSD1306_CHARGE_PUMP, cfg->charge_pump);
+			if (state != I2C_MASTER_TXDATA_ACK) {
+				break;
+			}
+		}
+
+		if (cfg->precharge != 0) {
+			state = ssd1306_send_command2(SSD1306_PRECHARGE_PERIOD, cfg->precharge);
+			if (state != I2C_MASTER_TXDATA_ACK) {
+				break;
+			}
+		}
+
+		if (cfg->segment_remap != 0) {
+			state = ssd1306_send_command(cfg->segment_remap);
+			if (state != I2C_MASTER_TXDATA_ACK) {
+				break;
+			}
+		}
+
+		if (cfg->com_remap != 0) {
+			state = ssd1306_send_command(cfg->com_remap);
+			if (state != I2C_MASTER_TXDATA_ACK) {
+				break;
+			}
+		}
+
+		if (cfg->com_config != 0) {
+			state = ssd1306_send_command2(SSD1306_COM_CONFIG, cfg->com_config);
+			if (state != I2C_MASTER_TXDATA_ACK) {
+				break;
+			}
+		}
+	} while (0);
+
+	ssd1306_stop();
+
+	return state;
+}
+
+i2c_state_t ssd1306_start(ssd1306_addr_t addr) {
+	i2c_state_t state;
+
+	i2c_start();
+
+	state = i2c_read_state_wait();
+	if (state != I2C_MASTER_START) {
+		return state;
+	}
+
+	i2c_slaW(addr << 1);
+
+	return i2c_read_state_wait();
+}
+
+i2c_state_t ssd1306_send_command2(ssd1306_command_t cmd, uint8_t data) {
+	i2c_state_t state = ssd1306_send_command(cmd);
+	if (state != I2C_MASTER_TXDATA_ACK) {
+		return state;
+	}
+	return ssd1306_send_command(cmd);
+}
+
+i2c_state_t ssd1306_send_command(ssd1306_command_t cmd) {
+	i2c_state_t state = ssd1306_send(CTRL_COMMAND);
+	if (state != I2C_MASTER_TXDATA_ACK) {
+		return state;
+	}
+	return ssd1306_send(cmd);
+}
+
+i2c_state_t ssd1306_send(uint8_t value) {
+	i2c_write(value);
+	return i2c_read_state_wait();
+}
+
+void ssd1306_stop() {
+	i2c_stop();
+	i2c_wait();
+}
--- drivers/ssd1306.h
+++ drivers/ssd1306.h
@@ -19,6 +19,8 @@ typedef enum {
 	SSD1306_DISPLAY_ON = 0xAF,
 	SSD1306_CLOCK_RATIO = 0xD5,
 	SSD1306_MULTIPLEX_RATIO = 0xA8,
+	SSD1306_DISPLAY_OFFSET = 0xD3,
+	SSD1306_DISPLAY_START_LINE = 0x40,
 	SSD1306_CHARGE_PUMP = 0x8D,
 	SSD1306_PRECHARGE_PERIOD = 0xD9,
 	SSD1306_DISPLAY_RAM = 0xA4,
@@ -26,42 +28,51 @@ typedef enum {
 	SSD1306_DISPLAY_NORMAL = 0xA6,
 	SSD1306_DISPLAY_INVERSE = 0xA7,
 	SSD1306_DISPLAY_CONTRAST = 0x81,
-	SSD1306_VCOM_DESELECT_LEVEL = 0xDB,
+	SSD1306_VCOMH_DESELECT_LEVEL = 0xDB,
+	SSD1306_COM_CONFIG = 0xDA,
 } ssd1306_command_t;
 
-inline i2c_state_t ssd1306_start(ssd1306_addr_t addr, ssd1306_control_t ctrl) {
-	i2c_state_t state;
+typedef enum {
+	EXTERNAL_VCC = 0x10,
+	INTERNAL_DC_DC = 0x14,
+} charge_pump_t;
 
-	i2c_start();
+typedef enum {
+	SEG_0_0 = 0xA0,
+	SEG_0_127 = 0xA1,
+} segment_remap_t;
 
-	state = i2c_read_state_wait();
-	if (state != I2C_MASTER_START) {
-		return state;
-	}
+typedef enum {
+	COM_0_COM_N = 0xC0,
+	COM_N_COM_0 = 0xC8,
+} com_remap_t;
 
-	i2c_slaW(addr << 1);
+typedef enum {
+	SEQUENTIAL = 0x02,
+	ALTERNATIVE = 0x12,
+	SEQUENTIAL_REMAP_COM_LR = 0x22,
+	ALTERNATIVE_REMAP_COM_LR = 0x32,
+} com_cfg_t;
 
-	return i2c_read_state_wait();
-}
+typedef struct {
+	uint8_t* display_clock;
+	uint8_t* multiplex_ratio;
+	uint8_t* display_offset;
+	uint8_t* display_start_line;
+	uint8_t* contrast;
+	uint8_t* vcomh_deselect_level;
+	charge_pump_t charge_pump;
+	charge_pump_t precharge;
+	segment_remap_t segment_remap;
+	com_remap_t com_remap;
+	com_cfg_t com_config;
+} ssd1306_config_t;
 
-inline i2c_state_t ssd1306_send(uint8_t value) {
-	i2c_write(value);
-	return i2c_read_state_wait();
-}
-
-inline i2c_state_t ssd1306_send_command(ssd1306_command_t cmd) {
-	i2c_state_t state;
-
-	state = ssd1306_send(CTRL_COMMAND);
-	if (state != I2C_MASTER_TXDATA_ACK) {
-		return state;
-	}
-	return ssd1306_send(cmd);
-}
-
-inline i2c_state_t ssd1306_stop() {
-	i2c_stop();
-	return i2c_read_state_wait();
-}
+extern i2c_state_t ssd1306_configure(ssd1306_addr_t addr, const ssd1306_config_t* cfg);
+extern i2c_state_t ssd1306_start(ssd1306_addr_t addr);
+extern i2c_state_t ssd1306_send_command2(ssd1306_command_t cmd, uint8_t data);
+extern i2c_state_t ssd1306_send_command(ssd1306_command_t cmd);
+extern i2c_state_t ssd1306_send(uint8_t value);
+extern void ssd1306_stop();
 
 #endif // SSD1306_H
